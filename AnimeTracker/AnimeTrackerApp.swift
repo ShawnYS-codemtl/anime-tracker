2416a561e97a44e07ea09cae96e23a88752bf185
ShawnYS-codemtl <syatsin@gmail.com>
2025-07-15 16:02:18 -0400

    Initial Commit

---
+++ AnimeTracker/AnimeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeTrackerApp.swift
+//  AnimeTracker
+//
+//  Created by Shawn Yat Sin on 2025-07-15.
+//
+
+import SwiftUI
+
+@main
+struct AnimeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimeTrackerTests/AnimeTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeTrackerTests.swift
+//  AnimeTrackerTests
+//
+//  Created by Shawn Yat Sin on 2025-07-15.
+//
+
+import Testing
+@testable import AnimeTracker
+
+struct AnimeTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
